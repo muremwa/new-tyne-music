2e2a528fb583a1ae71080fb7cea51e4ad1564333
Muremwa <danmburu254@gmail.com>
2021-07-29 10:38:26 +0300

    added docs

--- tyne/urls.py
+++ tyne/urls.py
@@ -2,12 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
+
+docs_description = """
+    This are the API end points available on tyne music
+"""
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # core/
     path('core/', include('core.urls')),
+
+    # docs/
+    path('docs/', include_docs_urls(title='Tyne Music API docs', description=docs_description))
 ]
 
 
