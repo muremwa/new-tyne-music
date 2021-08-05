db8fa9430706f76caaf8a4015fcc06df791d8fa0
Muremwa <danmburu254@gmail.com>
2021-08-05 11:56:37 +0300

    admin interface modified

--- tyne/settings.py
+++ tyne/settings.py
@@ -49,7 +49,7 @@ ROOT_URLCONF = 'tyne.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR / 'templates/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
